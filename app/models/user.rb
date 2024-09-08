e8e95afdab901ac0812781743ce5845f1715f6eb
Kadosaki Yoshiki <myutos44@gmail.com>
2024-09-08 19:50:28 +0900

    ニックネームの条件修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   has_many :reactions, dependent: :destroy
   has_many :notifications, dependent: :destroy
 
-  validates :nickname, presence: true, length: { in: 2..16 }
+  validates :nickname, presence: true, length: { maximum: 16 }
   validates :username, presence: true, length: { maximum: 16 }, uniqueness: { case_sensitive: false }, format: { with: /\w/, message: 'に使用できるのは、英数字とアンダーバーのみです。' }, on: :update
   validates :description, length: { maximum: 140 }
   validate :acceptable_image
