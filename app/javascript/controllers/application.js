f24c65ca4ad68aae453409ed58b0accf6f0e2c47
Kadosaki Yoshiki <myutos44@gmail.com>
2024-09-08 19:50:54 +0900

    投稿詳細ページでのリダイレクトの仕様追加

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,16 @@
 import { Application } from "@hotwired/stimulus"
 
 const application = Application.start()
+const { StreamActions } = Turbo
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+StreamActions.redirect = function () {
+  setTimeout(() => {
+    Turbo.visit(this.getAttribute("url"));
+  }, 2000);
+};
